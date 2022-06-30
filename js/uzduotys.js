a623a8c9381ae43c90d5dc6a4b6d9496cb1b9fde
Sergej <sergej11@yahoo.com>
2022-06-30 11:56:56 +0300

    Uzduotys_pradzia

---
+++ js/uzduotys.js
@@ -0,0 +1,39 @@
+// 1 uzduotis
+
+
+let skaicius1 = 3;
+let skaicius2 = 7;
+let skaicius3 = 10;
+console.log(skaicius1, skaicius2, skaicius3)
+
+let skaicius4 = 4;
+console.log(skaicius4);
+let skaicius5 = 8;
+console.log(skaicius5);
+let skaicius6 = 15;
+console.log(skaicius6);
+
+// 2 uzduotis
+
+const vilnius1 = "zirmunai sauletekis";
+console.log(vilnius1);
+const vilnius2 ='pasilaiciai justiniskes'
+console.log(vilnius2);
+const vilnius3 =`lazdynai naujamiestis`
+console.log(vilnius3);
+console.log(vilnius1, vilnius2, vilnius3);
+
+//3 uzduotis
+
+const suma = skaicius1 + skaicius2 + skaicius3;
+console.log(suma);
+
+//4 uzduotis
+
+const tarpas = "zirmunai pasilaiciai" + ' plytine sauletekis';
+console.log(tarpas);
+
+const eilute = `Zirmunai
+pasilaiciai
+justiniskes`;
+console.log(eilute);
