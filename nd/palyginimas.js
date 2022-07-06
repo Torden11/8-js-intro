ac1e7680d88afe96f4b32d73da27c20a0c3bdf51
Sergej <sergej11@yahoo.com>
2022-07-06 14:55:31 +0300

    palyginimas intermediate

---
+++ nd/palyginimas.js
@@ -0,0 +1,117 @@
+const a = 5;
+const b = 7;
+
+if  (a > b) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (a < b) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (a === b) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (a !== b) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (a >= b) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (a <= b) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+console.log("---------------------------------------");
+
+const salis1 = 'Lietuva';
+console.log (salis1.length);
+
+const salis2 = 'Estija';
+console.log (salis2.length);
+
+if  (salis1 > salis2) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (salis1 < salis2) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (salis1 === salis2) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (salis1 !== salis2) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (salis1 >= salis2) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+}
+
+if  (salis1 <= salis2) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite kita karta');
+};
+
+console.log("------------------------------");
+
+const salys = ["lietuva", 'uk', 'estija', 'usa'];
+console.log(salys[0].length, salys[1].length, salys[2].length, salys[3].length );
+
+
+
+/*function palyginimas(a, b) {
+    if  (a > b 
+    || a < b
+    || a === b
+    || a !== b)  {
+        return 'pomidoras';
+    }
+    else {
+        return'Bandykite kita karta';
+    }
+}
+
+console.log(palyginimas (8, 6));
+console.log(palyginimas (6, 6));
+*/
